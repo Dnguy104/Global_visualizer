93ee1bc678e91641fc0e7d841e169ce07a1005e7
Dnguy104 <dnguy104@ucr.edu>
2017-02-12 01:48:30 -0800

    color
    color

--- globe/globe.js
+++ globe/globe.js
@@ -18,7 +18,7 @@ DAT.Globe = function(container, opts) {
   
   var colorFn = opts.colorFn || function(x) {
     var c = new THREE.Color();
-    c.setHSL( ( 0.7 - ( x * 0.5 ) ), 1.0, 1.0 );
+    c.setHSL( ( 0.7 - ( x  ) ), 1.0, 1.0 );
     return c;
   };
   var imgDir = opts.imgDir || '/Users/danielnguyen/Documents/Dnguy104.github.io/globe';
